b9046b2f0999ef0d6dc5ddbb77d10906fc58716f
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-12-14 18:39:25 +0300

    changes frame components

--- src/components/Map/Options/FrameLayer.vue
+++ src/components/Map/Options/FrameLayer.vue
@@ -1,28 +1,25 @@
 <template>
-    <md-card md-with-hover class='fix' v-show="inShowParam" :style="{ width : inWidth+'px' }" >
-        
+    <md-card md-with-hover class='fix' v-show="inShowParam" :style="{ width : inWidth+'px' }" >    
         <md-card-header class='frameLayerHeader' align="right" >
-
             <table class='fullSize' >
                 <tr>
                     <td>
                         <mButton
-                                v-show='isSave' 
-                                @click='setClickSaveBtn()' > 
-                            <img class='imgInButton' :src="require('content/images/save.png')" >
+                            v-show='isSave' 
+                            @click='setClickSaveBtn()' > 
+                                <img class='imgInButton' :src="require('content/images/save.png')" >
                         </mButton>
                     </td>
                     <td align='right'>
                         <mButton
-                                @click='setShowMode(showName)'
-                                v-show='isExit' > 
-                            <img class='imgInButton' :src="require('content/images/close.png')" >
+                            @click='setShowMode(showName)'
+                            v-show='isExit' > 
+                                <img class='imgInButton' :src="require('content/images/close.png')" >
                         </mButton>
                     </td>
                 </tr>
             </table>
         </md-card-header>
-
         <md-card-expand class='frameLayerContent fullSize' >
             <slot/>
         </md-card-expand>        
@@ -30,35 +27,34 @@
 </template>
 
 <script>
-
     import { mapMutations, mapActions } from 'vuex'
     import mButton from 'components/Additional/MButton'
-    import Mover   from 'services/Mover'
-
+    import Mover from 'services/Mover'
     export default {
-
         components : { mButton },
         props : [ 'inWidth', 'inShowParam', 'showName', 'isExit', 'isSave' ],
-        mounted : ()=> Mover.moveAllObj(),
-
+        mounted : ()=> {
+            Mover.moveAllObj();
+        },
         methods : {
-
             ...mapMutations(['setShowMode', 'setCurrentMapValue']),
-
-            setClickSaveBtn(){
-
-                this.setShowMode("activeModal");
+            setClickSaveBtn() {
+                this.setShowMode('activeModal');
                 this.setCurrentMapValue({ field : 'activeModalTitle', value : this.$lang.messages.objectDialogInfo });
+                this.setCurrentMapValue({ 
+                    field : 'activeConfrmDialogAction', 
+                    value : {
+                        nameAction : 'setStartSaveModal',
+                        //dataAction : isSaveAction
+                    } 
+                });
             }
         }
     }
 </script>
-
 <style>
-    
-    .frameLayerHeader, .frameLayerContent{
-        
-        padding  : 3px;
-        cursor   : all-scroll;
+    .frameLayerHeader, .frameLayerContent{ 
+        padding : 3px;
+        cursor : all-scroll;
     }
 </style>
