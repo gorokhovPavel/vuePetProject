942890e724477ff1fdb56f8a0822a943880dad82
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-12-17 15:58:25 +0300

    ref2

--- src/components/Additional/Table/GridTable.vue
+++ src/components/Additional/Table/GridTable.vue
@@ -1,18 +1,20 @@
 <template>
-    <table class='fullSize gridTemplate' v-if='columnsBody' cellspacing='0' cellpadding='0' ref='dataTable'>
+    <table class='fullSize gridTemplate' >
         <thead>
             <tr>
-                <th v-for='(item, i) in columnsTable' :key='i'
-                    @click='sortBy(columnsBody[i])' 
-                    :class='{ active : sortKey === columnsBody[i] }' >
-                        <span> {{ item.value | capitalize }} </span>
-                        <span class="arrow" v-if='item.isOrder' :class="sortOrders[columnsBody[i]] > 0 ? 'asc' : 'dsc'"></span>
+                <th v-for='(item, i) in columnsData' :key='i'
+                    @click='setSortBy(columnsNames[i])' 
+                    :class='{ active : sortKey === columnsNames[i] }' >
+                        <span> 
+                            {{ item.value | capitalize }} 
+                        </span>
+                        <span class='arrow' v-if='item.isOrder' :class='getSortStyle(i)' />
                 </th>
             </tr>
         </thead>
-        <tbody>
-            <tr v-for='entry in filteredData' :key='JSON.stringify(entry)' >
-                <gridTd v-for='item in columnsBody' :key='JSON.stringify(item)'
+        <tbody v-if='isExistDataRows' >
+            <tr v-for='(entry, indexFirst) in filteredData' :key='indexFirst' >
+                <gridTd v-for='(item, indexSecond) in columnsNames' :key='indexSecond'
                     :allObjTr='entry'
                     :tableNameState='tableNameInState'
                     :column='item'
@@ -21,155 +23,126 @@
                 />
             </tr>
         </tbody>
+        <tbody v-else >
+            <tr>
+                <td :colspan='columnsData.length' align='center' class='noData' >
+                    {{ $lang.messages.noDataTable }}
+                </td>
+            </tr>
+        </tbody>
     </table>
 </template>
-
 <script>
-
     import { mapActions, mapMutations, mapGetters } from 'vuex'
     import gridTd from 'components/Additional/Table/GridTd'
-    
     export default {
-
         components : { gridTd },
-
-        props : [ 'dataTable', 'columnsTable', 'filterKey', 'tableNameInState' ],
-
+        props : [ 'rowsData', 'columnsData', 'filterKey', 'tableNameInState' ],
         data :() => ({
-
-            sortKey    : '',
+            sortKey : '',
             sortOrders : {},
         }),
-
+        created() {  
+            const tempSortKey = {};
+            this.sortOrders = tempSortKey;
+            this.columnsNames.forEach( key => tempSortKey[key] = 1 );
+        },
         computed : {
-
-            ...mapGetters([ 'getAllMapState', 'getLineOrPolygon' ]),
-
-            columnsBody(){
-
-                return Object.keys(this.dataTable[0]);
+            ...mapGetters([ 'getAllMapState', 'getLineOrPolygon', 'getInstrument' ]),
+            isExistDataRows() {
+                return this.rowsData.length > 0  || false;
             },
+            columnsNames() {
+                const tableDataArr = this.rowsData.length>0 ? 
+                    Object.keys(this.rowsData[0]) : 
+                    this.columnsData.map( item=> item.value );
+                return tableDataArr;
+            },
+            filteredData() {    
+                const sortKey = this.sortKey;
+                const order = this.sortOrders[sortKey] || 1;
+                const filterKey = this.filterKey && this.filterKey.toLowerCase();
+                const rowsInState = this.getAllMapState[this.tableNameInState];
+                let rowsArr = rowsInState || this.rowsData;
 
-            filteredData(){
-
-                let sortKey     = this.sortKey;
-                let order       = this.sortOrders[sortKey] || 1;
-                let filterKey   = this.filterKey && this.filterKey.toLowerCase();
-                let rowsInState = this.getAllMapState[this.tableNameInState];
-                let rows        = rowsInState || this.dataTable;
-
-                if( !rowsInState ){
-
-                    //Записываем текущее состояние таблицы в стейт
+                if( !rowsInState ) {
                     this.setCurrentMapValue({
                         field : this.tableNameInState,
-                        value : rows
+                        value : rowsArr
                     });
                 }
-
-                if ( filterKey ) {
-                    rows = rows.filter( row =>
+                if( filterKey ) {
+                    rowsArr = rowsArr.filter( row =>
                         Object.keys( row ).some( key => String( row[key] ).toLowerCase().indexOf(filterKey) > -1 )
                     );
                 }
-
-                if ( sortKey ) {
-                    rows = rows.slice().sort( (a, b) => {
-
+                if( sortKey ) {
+                    rowsArr = rowsArr.slice().sort( (a, b) => {
                         a = a[sortKey];
                         b = b[sortKey];
-                        
                         return (
                             a === b ? 
                                 0 : (a > b) ?
                                     1 : -1 ) * order;
                     });
                 }
-
-                return rows;
+                return rowsArr;
             },
         },
-
-        created(){
-            
-            let tempSortKey = {};
-            this.columnsBody.forEach( key => tempSortKey[key] = 1 );
-            this.sortOrders = tempSortKey;
-        },
-
         filters : {
-
             capitalize : str => str.charAt(0).toUpperCase() + str.slice(1)
         },
-        
         methods : {
-
             ...mapMutations(['setCurrentMapValue']),
-
-            sortBy : function(key){
-
+            setSortBy(key) {
                 this.sortKey = key;
                 this.sortOrders[key] = this.sortOrders[key] * -1;
-            }
+            },
+            getSortStyle(index) {                
+                return this.sortOrders[this.columnsNames[index]] > 0 ? 'asc' : 'dsc';
+            },
         }
     }
 </script>
-
 <style>
-
-    .gridTemplate th {
-
-        background-color: #00338d;
-        color: rgba(255,255,255,0.66);
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        -ms-user-select: none;
-        user-select: none;
-        padding: 15px;
+    .gridTemplate {
+        margin : 10px auto;
+        border-collapse : collapse;
     }
-
-    .gridTemplate th.active {
-
-        color: #fff;
+    .gridTemplate thead {
+        border-top : solid 2px #00338d;
+        border-bottom : solid 2px #00338d;
     }
-
-    .gridTemplate th.active .arrow {
-
-        opacity: 1;
+    .gridTemplate thead th {
+        padding : 15px 10px;
     }
-
     .gridTemplate td {
-
-        text-align : center;
-        padding    : 3px;
+        padding : 2px;
+        border-top : solid 1px #eee;
     }
-
     .gridTemplate tr:hover td {
-
-        background-color :lightgray;
+        background-color : #eee;
     }
-
     .arrow {
-
-        display: inline-block;
-        vertical-align: middle;
-        width: 0;
-        height: 0;
-        margin-left: 2px;
-        opacity: 0.66;
+        display : inline-block;
+        vertical-align : middle;
+        width : 0;
+        height : 0;
+        margin-left : 2px;
+        opacity : 0.66;
     }
-
     .arrow.asc {
-
-        border-left: 4px solid transparent;
-        border-right: 4px solid transparent;
-        border-bottom: 4px solid #fff;
+        border-left : 5px solid transparent;
+        border-right : 5px solid transparent;
+        border-bottom : 5px solid #00338d;
     }
-
     .arrow.dsc {
-
-        border-left: 4px solid transparent;
-        border-right: 4px solid transparent;
-        border-top: 4px solid #fff;
+        border-left: 5px solid transparent;
+        border-right: 5px solid transparent;
+        border-top: 5px solid #00338d;
+    }
+    .noData {
+        line-height : 52px;
+        background-color: #ededed;
     }
 </style>
