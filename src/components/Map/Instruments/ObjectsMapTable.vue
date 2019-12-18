a24195a44fc380fcc21b0ed5a79862fdfd4fdaca
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-12-18 07:47:05 +0300

    change components of instrument

--- src/components/Map/Instruments/ObjectsMapTable.vue
+++ src/components/Map/Instruments/ObjectsMapTable.vue
@@ -1,128 +1,64 @@
 <template>
-    <div>
-        <gridFilter v-show='getShowingObjects.showTableMap'
-            :tableName='curTableName' 
-            :tableRows='computedTableData'
-            :tableCols='computedColumnsData'>
-                <table class='fullSize' >
-                    <tr>
-                        <td align='left' >
-                            <mButton @click='setActionMapObj(false)' > 
-                                <img class='imgInButton' :src="require('content/images/cancel.png')" >
-                            </mButton>
-                        </td>
-                        
-                        <td align='center' >
-                            
-                            <!-- {{infoMapLayer}} -->
-                            
-                            <mButton @click='setChangeModeActiveMapObj("isMarkersActive")' :class='getClassForBoard("isMarkersActive")' > 
-                                <img class='imgInButton' :src="require('content/images/point.png')" >
-                            </mButton>
-
-                            <mButton @click='setChangeModeActiveMapObj("isMeasureActive")' :class='getClassForBoard("isMeasureActive")' > 
-                                <img class='imgInButton' :src="require('content/images/polygon.png')" >
-                            </mButton>
-
-                            <mButton @click='setActionChangeDrawMode("isDrawActive")' :class='getClassForBoard("isDrawActive")' v-show='getMapObjProperties.isDrawShow' > 
-                                <img class='imgInButton' :src="require('content/images/drawMeasure.png')" >
-                            </mButton>
-                        </td>
-
-                        <td align='right' >
-                            <mButton @click='setActionMapObj(true)' > 
-                                <img class='imgInButton' :src="require('content/images/save.png')" >
-                            </mButton>
-                        </td>
-                    </tr>
-                </table>
-        </gridFilter>
-    </div>
+    <gridFilter v-show='getShowingObjects.showTableMap' :tableName='curTableName' >
+        <table class='fullSize' >
+            <tr>
+                <td align='left' >
+                    <mButton @click='setActionMapObj(false)' > 
+                        <img class='imgInButton' :src="require('content/images/cancel.png')" >
+                    </mButton>
+                </td>
+                <td align='center' >
+                    <mButton @click='setActiveAllDraws()' :class='getClassForBoard("isMarkersActive")' > 
+                        <img class='imgInButton' :src="require('content/images/alldraws.png')" >
+                    </mButton>
+                    <mButton @click='setChangeModeActiveMapObj("isMarkersActive")' :class='getClassForBoard("isMarkersActive")' > 
+                        <img class='imgInButton' :src="require('content/images/point.png')" >
+                    </mButton>
+                    <mButton @click='setChangeModeActiveMapObj("isMeasureActive")' :class='getClassForBoard("isMeasureActive")' > 
+                        <img class='imgInButton' :src="require('content/images/polygon.png')" >
+                    </mButton>
+                    <mButton @click='setActionChangeDrawMode("isDrawActive")' :class='getClassForBoard("isDrawActive")' v-show='getMapObjProperties.isDrawShow' > 
+                        <img class='imgInButton' :src="require('content/images/drawMeasure.png')" >
+                    </mButton>
+                </td>
+                <td align='right' >
+                    <mButton @click='setActionMapObj(true)' > 
+                        <img class='imgInButton' :src="require('content/images/save.png')" >
+                    </mButton>
+                </td>
+            </tr>
+        </table>
+    </gridFilter>
 </template>
 
 <script>
-    
-    import { mapGetters, mapMutations, mapActions } from 'vuex' 
-    import gridFilter from 'components/Additional/Table/GridFilter'
-    import mButton    from 'components/Additional/MButton'
-import { log } from 'async';
-
+    import { mapGetters, mapMutations, mapActions } from 'vuex';
+    import gridFilter from 'components/Additional/Table/GridFilter';
+    import mButton from 'components/Additional/MButton';
     export default {
-
         components : { gridFilter, mButton },
-        
-        props : [ 'tableData', 'tableName' ],
-
+        props : [ 'tableName' ],
         data :()=>({
-
             modalSureSaveName : '',
-            curTableData : null,
-            curTableName : null,
         }),
-
-        created(){
-
-            this.curTableData = this.tableData.map( x => x.properties );
+        created() {
             this.curTableName = this.tableName;
-            this.getInstrument.setImagesForDrawObjects();
         },
-
         computed : {
-
             ...mapGetters([ 
-                'getLayersIndexes', 
                 'getShowingObjects', 
-                'getLineOrPolygon', 
-                'getLocalMap', 
                 'getInstrument', 
                 'getClassForBoard', 
                 'getMapObjProperties' 
             ]),
-
-            computedTableData(){
-
-                return this.curTableData.map( x => {
-
-                    let currType = x.typeView || x.type;
-                    let currIsReport = this.getLineOrPolygon(currType) ? x.isReport : true;
-                    let currNumColor = x.numColor || ( this.getLineOrPolygon(currType) ? 3 : 6  );
-                    
-                    return {
-                        type     : currType,
-                        name     : x.name,
-                        date     : x.data,
-                        numColor : currNumColor,     
-                        isReport : currIsReport,
-                        delete   : null,
-                        id       : x.id
-                    }
-                });
-            },
-            
-            computedColumnsData(){
-                return [
-                    { isOrder : true,  value : '' },
-                    { isOrder : true,  value : this.$lang.messages.tableHeaderName },
-                    { isOrder : true,  value : this.$lang.messages.tableHeaderDate },
-                    { isOrder : true,  value : '' },
-                    { isOrder : true,  value : '' },
-                    { isOrder : false, value : '' },
-                ]
-            }
         },
-
         methods : {
-
             ...mapActions([ 'setActionChangeDrawMode' ]),
             ...mapMutations([ 'setCurrentMapValue', 'setShowMode' ]),
-
-            setActionMapObj( isSaveAction ){
-            
+            setActionMapObj( isSaveAction ) {
                 this.modalSureSaveName = isSaveAction ? this.$lang.messages.sureSaveMapObj : this.$lang.messages.sureCancelMapObj;
-        
-                this.setCurrentMapValue({ field : 'activeModal',      value : true });
-                this.setCurrentMapValue({ field : 'activeModalTitle', value : this.modalSureSaveName });
-                
+                this.setCurrentMapValue({ field : 'activeModal', value : true });
+                this.setCurrentMapValue({ field : 'activeModalTitle', value : this.modalSureSaveName });  
                 this.setCurrentMapValue({ 
                     field : 'activeConfrmDialogAction', 
                     value : {
@@ -131,12 +67,15 @@ import { log } from 'async';
                     } 
                 });
             },
-
-            setChangeModeActiveMapObj(inStateElem){
-
+            setChangeModeActiveMapObj( inStateElem ) {
                 this.setShowMode(inStateElem);
                 this.getInstrument.setImagesForDrawObjects();
             },
+            setActiveAllDraws() {
+                this.setChangeModeActiveMapObj("isMarkersActive");
+                this.setChangeModeActiveMapObj("isMeasureActive");
+                this.setActionChangeDrawMode("isDrawActive");
+            }
         }
     }
 </script>
