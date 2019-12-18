d8246aec151f36b5149e6d34fca26a62a68b2a7c
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-12-18 07:46:21 +0300

    change components of instrument

--- src/components/Map/Instruments/MeasGraphMap.vue
+++ src/components/Map/Instruments/MeasGraphMap.vue
@@ -4,44 +4,68 @@
             @mousedown='setMouseDownUpOnGraph(false)' 
             @mouseleave='setMouseDownUpOnGraph(true)'
             :style="{ 
-                width  : (getFrameContentWidthPx(35)-20)+'px', 
+                width : (getFrameContentWidthPx(35)-20)+'px', 
                 height : (getFrameContentWidthPx(35)/2)+'px' 
             }">
         </div>   
-        
-        <tbl2Buttons id='graphChartSet'
-            :firButName='$lang.messages.mainLayerName'
-            :secButName='$lang.messages.addLayerName'
-            @firButClick='setVisible3d(true)'
-            @secButClick='setVisible3d(false)'>
-                <p id='graphChartInfo'></p>
-        </tbl2Buttons>
+        <table class='fullSize' border='0' v-show='getShowingObjects.show3DPanel' >
+            <tr>
+                <td colspan="2" align='center' >
+                    <div id='graphChartInfo' ></div>
+                </td>
+            </tr>
+            <tr>
+                <td valign='bottom' align='left' width='50%' :v-if='false' >
+                    <mButton @click='setVisible3d(true)' :class="{ onBord : isMainButActive, hideBord : !isMainButActive }" > 
+                        {{$lang.messages.mainLayerName}}
+                    </mButton> 
+                </td>
+                <td valign='bottom' align='right' width='50%' v-show='getShowingObjects.isActiveAddLayer' >
+                    <mButton @click='setVisible3d(false)' :class="{ onBord : isAddButActive, hideBord : !isAddButActive }" > 
+                        {{$lang.messages.addLayerName}}
+                    </mButton> 
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
 
 <script>
-
     import { mapActions, mapGetters } from 'vuex'
-    import tbl2Buttons from 'components/Additional/BottomTable2Buttons'
-
+    import buildChart from 'services/BuildChart'
+    import mButton from 'components/Additional/MButton'
     export default {
-
-        components : { tbl2Buttons },
-
+        components : { mButton },
+        data : ()=>({
+            isMainButActive : true,
+            isAddButActive : true
+        }),
         computed : {
-
-            ...mapGetters([ 'getFrameContentWidthPx', 'getMediaData' ]),
+            ...mapGetters([ 'getFrameContentWidthPx', 'getShowingObjects' ]),
         },
+        methods : {  
+            setVisible3d( isMain ) {
+                const myDivObj = document.querySelector('#graphPlotChart');
+                const [ indexLayer, nameLayer ] = isMain ? [ 0, 'isMainButActive' ] : [ 1, 'isAddButActive' ];
 
-        methods : {
-            
-            ...mapActions( [ 'setVisible3d', 'setMouseDownUpOnGraph' ] ),
+                if( !myDivObj.data ) 
+                    return;
+
+                const visGraph = myDivObj.data[indexLayer].visible;
+                [ myDivObj.data[indexLayer].visible, this[nameLayer] ] = 
+                    ( !visGraph  || visGraph === 'visibility' ) ?  [ 'legendonly', false ] : [ 'visibility', true ];
+
+                buildChart.setResstartChart(myDivObj);
+            },
+            setMouseDownUpOnGraph( isUp ) {
+                const parentFrame = document.querySelector('.measGraphMapPos');
+                if ( parentFrame.classList ) {
+                    if ( isUp )
+                        parentFrame.className = parentFrame.className + ' move';
+                    else 
+                        parentFrame.className = parentFrame.className.replace(/\bmove\b/g, '');
+                }
+            },
         }
     }
-
 </script>
-
-<style>
-    
-
-</style>
