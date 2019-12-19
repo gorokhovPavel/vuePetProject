886f990b6b09c803411c154b924676c5f43a50f4
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-12-19 13:59:09 +0300

    ref eslint

--- src/components/Additional/BottomTable2Buttons.vue
+++ src/components/Additional/BottomTable2Buttons.vue
@@ -2,17 +2,15 @@
     <table class='fullSize' border='0'>
         <tr>
             <td valign='bottom' align='left' width='15%' :v-if='false' >
-                <mButton :style='{ display : isFirBut }' @click='mButClick' > 
+                <mButton class='indentButt' :style='{ display : isFirBut }' @click='mButClick' > 
                     {{ firButName }}
                 </mButton> 
             </td>   
-            
             <td align='center' width='70%' >
                 <slot />
             </td>
-            
             <td valign='bottom' align='right' width='15%' >
-                <mButton :style='{ display : isSecBut }' @click='aButClick' > 
+                <mButton class='indentButt' :style='{ display : isSecBut }' @click='aButClick' > 
                     {{ secButName }}
                 </mButton> 
             </td>
@@ -21,31 +19,22 @@
 </template>
 
 <script>
-
     import mButton from './MButton';
     export default {
-
         components : { mButton },
         props : [ 'firButName', 'firButClick', 'secButName', 'secButClick' ],
-
-        data : ()=>({
-
+        data : ()=> ({
             isFirBut : 'none',
             isSecBut : 'none',
         }),
-
-        created(){
-
+        created() {
             this.isFirBut = this.firButName ? 'block' : 'none';
             this.isSecBut = this.secButName ? 'block' : 'none';
         },
-
         methods : {
-
             mButClick(event){
                 this.$emit('firButClick', event);
             },
-
             aButClick(event){
                 this.$emit('secButClick', event);
             }
@@ -54,5 +43,7 @@
 </script>
 
 <style >
-    
+    .indentButt {
+        margin: 5px 0;
+    }
 </style>
