4154aaf015fec654e69dd28f1807ac7a7721880f
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2019-12-23 15:15:47 +0300

    ref old files

--- src/services/AddExtension.js
+++ src/services/AddExtension.js
@@ -1,7 +1,6 @@
 export default class AddExtension {
 
-    static getListColor(){
-
+    static getListColor() {
         return [
             { id : 1, value : '#ff0000' },
             { id : 2, value : '#00ff00' },
@@ -9,42 +8,49 @@ export default class AddExtension {
             { id : 4, value : '#ffff00' },
             { id : 5, value : '#ffa500' },
             { id : 6, value : '#808080' },
-        ]
+        ];
     }
 
     //Возврат значения параметра по номеру в списке
-    static getValueFromNum( listOfParam, inNumOfColor ){
-    
+    static getValueFromNum( listOfParam, inNumOfColor ) {
         let elemColor = listOfParam.find( x => x.id === inNumOfColor );
         let goodElemColor = elemColor || listOfParam[0];
         return goodElemColor.value;
     }
 
     //Оборачиваем запрос в лоадер и записываем ошибку в консоль
-    static extForAxiosWithState( commit, { letUrlAction, goodCallBack, badCallBack } ){
-  
+    static setForAxiosWithState( commit, { letUrlAction, goodCallBack, badCallBack } ) {
+        //Включаем лоадер. Если обработчики не проставлены - просто заносим ошибку в консоль и отключаем лоадер
         commit( 'setCurrentMapValue', { field : 'loading',  value : true } );
-        
-        //Если обработчики не проставлены - просто заносим ошибку в консоль и отключаем лоадер
         letUrlAction.then(
-          response => this._setRequestProcessing( commit, goodCallBack, response ),
-          reject   => this._setRequestProcessing( commit, badCallBack, reject.response ),
+            response => this._setRequestProcessing( commit, goodCallBack, response ),
+            reject => this._setRequestProcessing( commit, badCallBack, reject.response ),
         );
+    }
+
+    //Выводим ошибку в уведомлялку, скрываем чз некоторе время
+    static setErrorNotification( commit, inErrorText ){
+
+        commit( 'setCurrentMapValue', { field : 'activeSnack', value : true });
+        commit( 'setCurrentMapValue', { field : 'activeSnackError', value : true });
+        commit( 'setCurrentMapValue', { field : 'activeSnackTitle', value : inErrorText });
+        
+        setTimeout( ()=>{
+          commit( 'setCurrentMapValue', { field : 'activeSnack', value : false });
+        }, 2000 );
     }
 
     //Если обработчик ошибок не проставлен - просто заносим ошибку в консоль и отключаем лоадер
     static _setRequestProcessing( commit, inSetFun, answer ){
-
         //Проверка на обработчик
-        if( inSetFun )
+        if( inSetFun ) {
             inSetFun( answer );
-        else    
+        } else {
             console.log( answer ); 
-
+        }
         commit( 'setCurrentMapValue', { field : 'loading',  value : false } );
         
-        if( !answer )
-            return;
+        if( !answer ) return;
         
         //А-ля интерсептор, провкерка на отсутствие прав доступа
         if( answer.status === 401 )
@@ -52,46 +58,44 @@ export default class AddExtension {
     }
 
     //Из глобального объекта mapbox-gl-draw тянем текущий выбранный, если есть
-    static getSelectDraw(draw){
-
+    static getSelectDraw(draw) {
         let selectesDrawItem = null;
-
         try{
             selectesDrawItem = draw.getSelected().features[0];
         } catch(error) {
-
             console.log(error);
         }
-
         return selectesDrawItem;
     }
 
-    //Смотрим, какой греппе маркеров или инструментов измерения принадлежит текущий объект
-    static getTypeObjForDraw( draw, listOfInst, numOfInstrument ){
+    //Смотрим, какой группе маркеров или инструментов измерения принадлежит текущий объект
+    static getTypeObjForDraw( draw, listOfInst ) {
 
         let typeDraw = null;
+        let itemInList = null;
 
-        if( draw.properties.typeView || draw.properties.type )
-            typeDraw = draw.properties.typeView ? draw.properties.typeView : draw.properties.type;
-        
-        else if ( draw.geometry.type === 'LineString' )
-            typeDraw = 'line'
-        
-        else 
-            typeDraw = draw.geometry.type.toLowerCase();
+        //Смотрим на properties
+        if( draw.properties ) {
+            if( draw.properties.typeView || draw.properties.type ) 
+                typeDraw = draw.properties.typeView ? draw.properties.typeView : draw.properties.type;
+        } 
+        //Смотрим на geometry
+        if( !typeDraw ) {
+            if ( draw.geometry.type === 'LineString' ) {
+                typeDraw = 'line';
+            } else 
+                typeDraw = draw.geometry.type.toLowerCase();
+        }
 
-        const itemInList = listOfInst.filter( x => x.name === typeDraw );
+        itemInList = listOfInst.filter( x => x.name === typeDraw );
         return itemInList[0];
     }
 
     //Чистим дубли в массиве ( КО :)
-    static getArrWithoutDublicate(inArr){
-
-        let uniqArr = inArr.filter( (item, i, self)=> { 
-            
+    static getArrWithoutDublicate(inArr) {
+        let uniqArr = inArr.filter( (item, i, self)=> {  
             return i === self.indexOf(item); 
         });
-
         return uniqArr;
     };
 
@@ -107,33 +111,24 @@ export default class AddExtension {
 
     //Вытягиваем высоты из результата по координатам
     static setParseJsonToData( inJson, zeroMark ) {
-
-        // Входной объект может сразу содержать значения или в только в блоке results
+        //Входной объект может сразу содержать значения или в только в блоке results
         inJson = inJson.results ? inJson.results : inJson;
-        
-        if(inJson.features)
+        if( inJson.features )
             inJson = inJson.features.map( x=>x.properties );
-
-        let heightCoord = inJson.map( item=>{
-            
-            if( item.height !== null || item.height !== undefined ){
-    
+        let heightCoord = inJson.map( item=> {
+            if( item.height !== null || item.height !== undefined ) {
                 item.height -= zeroMark;
                 return item.height;
             }
         });
-    
         return heightCoord;
     };
 
     //Переключение видимости в DOM-e
-    static setToggleElem(idName, isHidden){
-
+    static setToggleElem(idName, isHidden) {
         let selElem = document.querySelector(`#${idName}`);
         let setElemDisp = selElem.style.display;
-
-        if( selElem ){
-
+        if(selElem) {
             if(!isHidden)
                 selElem.style.display = (setElemDisp != 'none') ? 'none' : 'block';
             else
@@ -142,19 +137,28 @@ export default class AddExtension {
     };
   
     //Фильтр...
-    static getSearchByValue(items, term){
-
-        if(items)
+    static getSearchByValue(items, term) {
+        if(items) {
             return !term ? items : items.filter(
                 item => this.getToLower(item.name)
                     .includes(this.getToLower(term))
             );
+        }
     }
 
     //...по нижнему регистру
-    static getToLower(text){
-
-        if(text)
+    static getToLower(text) {
+        if( text ) {
             return text.toString().toLowerCase();
+        }
+    }
+
+    //тянем текущую дату
+    static getCurrentDate() {
+        const today = new Date();
+        const dd = String(today.getDate()).padStart(2, '0');
+        const mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+        const yyyy = today.getFullYear();
+        return `${dd}.${mm}.${yyyy}`;
     }
 }
