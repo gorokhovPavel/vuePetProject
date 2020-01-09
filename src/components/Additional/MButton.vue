38795a7122169226ea66c5dbcf7838f358794248
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2020-01-09 17:26:58 +0300

    lint changes

--- src/components/Additional/MButton.vue
+++ src/components/Additional/MButton.vue
@@ -1,16 +1,21 @@
 <template>
-    <md-button @click="$emit('click')" 
-        class='minMdButton md-card md-with-hover' >
-            <slot/>
-    </md-button>
+    <div @click="$emit('click')" 
+        class='mdButton point'  >
+            <slot />
+    </div>
 </template>
-
-<script>
-    
-</script>
-
 <style>
-    .minMdButton {
-        min-width : 10px;
+    .mdButton {
+        display : inline-block;
+        margin : 7px;
+        padding : 7px;
+        box-shadow: 0 1px 1px rgba(0,0,0,0.3);
+    }
+    .mdButton:hover {
+        box-shadow: 2px 2px 2px rgba(0,0,0,0.3);
+    }
+    .mdButton img {
+        width : 20px;
+        height : 20px;
     }
 </style>
