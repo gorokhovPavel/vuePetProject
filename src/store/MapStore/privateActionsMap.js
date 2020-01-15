babda77cea3c46a4df12f2a1041370ae22e01c2d
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2020-01-15 23:11:53 +0300

    change store files

--- src/store/MapStore/privateActionsMap.js
+++ src/store/MapStore/privateActionsMap.js
@@ -1,117 +1,115 @@
 import api from 'api/apiConfig'
 import ext from 'services/AddExtension'
 import Layer from 'services/Layer'
+import lang  from 'language/Translate'
 
 const privateActionsMap = {
-    
-    _setLoadMapObjects( { state, commit } ){
-      
-      ext.extForAxiosWithState( commit, {
-    
-        letUrlAction : api.getUserMapObjects( state.mapId ), 
-        goodCallBack : response => {
 
-          if( !response.data ) 
-            return;
-            
-          const resObjList = JSON.parse( response.data.mapObjListJson );
-  
-          if ( resObjList.length > 0 ){
-            
-            commit('setCurrentMapValue', { field : 'mapObjListServer', value : resObjList });
-
-            state.mapObjListDraw.add({
-              type : 'FeatureCollection',
-              features : resObjList
-            });
-          }
-        }
-      });
-    },
-
-    _setRenderDraw( { state, commit, getters } ){
-
-      commit( 'setCurrentMapValue', { field : 'mapObjListDraw',  value : getters.getInstrument.getMapBoxDraw() });
-      this._setLoadMapObjects( { state, commit, getters } )
-      state.curMap.addControl( state.mapObjListDraw );
-    },
-    
     //Перегрузка таблицы с объектами, и перерисовка самих объектов
-    _setReloadMapObj( { state, commit, getters } ){
-
+    _setReloadMapObj( { state, commit, getters } ) {
       commit( 'setMapObjListDefault' );
-      this._setRenderDraw( { state, commit, getters } );
+      this._setRenderDraw({ state, commit, getters });
       commit('setCurrentMapValue', { field : 'isDrawShow', value : true } );
     },
 
-    _setRenderLayers( { state, commit } ){
-      
-      const inMap = state.curMap;
+    //Рендерим объекты измерения на карту
+    _setRenderDraw( { state, commit, getters } ) {
+      //Активируем начальный элемент mapBoxDraw, что ответчает за пользовательское управление элементами карты
+      commit( 'setCurrentMapValue', { field : 'mapObjListDraw', value : getters.getInstrument.getMapBoxDraw() });
+      //Выполняем запрос, что вернет массив объектов с готовыми пользовательскими элементами
+      this._setLoadMapObjects({ state, commit })
+        .then( result=> {
+            //Закрасим их
+            getters.getInstrument.setImagesForDrawObjects();
+            return result;
+          }
+        ).then( result=> {
+          //Добавим коллекцию из БД в элемент mapBoxDraw ( без setTimeout не обойтись, надо асинхронно, фоном грузить элементы )
+         setTimeout(()=>{
+          state.mapObjListDraw.add({
+            type : 'FeatureCollection',
+            features : result
+          });
+         }, 1 );
+         return state.mapObjListDraw;
+        }).then( result=> {
+          //Ну а после добавим заполненный mapBoxDraw базовыми элементами на главную карту
+          state.curMap.addControl( result );
+        });
+    },
 
-      //Eсли не заполнены основные свойства - выходим, нет смысла формировать объект Layer
-      if( !this._getCheckMainMapProperty(inMap) )
-        return;
+    //Выгружаем json объектов измерения из БД
+    _setLoadMapObjects( { state, commit } ) {
+      return new Promise( resolve=> {
+        ext.setForAxiosWithState( commit, {
+          letUrlAction : api.getUserMapObjects( state.mapId ), 
+          goodCallBack : response => {
+            if( !response.data ) return;
+            try {
+              const resObjList = JSON.parse( response.data.mapObjListJson );
+              commit( 'setCurrentMapValue', { field : 'mapObjListServer', value : resObjList } );
+              resolve(resObjList);
 
-      //Формируем 3д модель
-      //Слишком много памяти сжирается из-за фоновой загрузки 3д
-      commit( 'set3dFrame', state.mapModel );
-
-      //Формируем отснятые слои
-      let layerItem = 
-        new Layer( inMap, state.mapModel, state.showColorLayer, state.currentMainIndex, state.currentAddIndex, state.opacityOfLayer );
-  
-      layerItem.setStartOptionsOfLayers();
-
-      //Чистим ресурсы 
-      layerItem = null;
-      commit('setLayersRenderDisable');
+              // if ( resObjList.length > 0 ) {
+              //   commit( 'setCurrentMapValue', { field : 'mapObjListServer', value : resObjList } );
+              //   resolve(resObjList);
+              // }
+            } catch(err) {
+              console.log(err);
+            }
+          }
+        });
+      });
     },
   
     //Смотрим, сформированы ли основные свойства объекта карты
-    _getCheckMainMapProperty( inObjMap ){
-
+    _getCheckMainMapProperty( inObjMap ) {
       let isExistStyleInMap = true;
-
       try {
-        
         inObjMap.getStyle();
         inObjMap.getLayer();
       } catch(err){
-        
         isExistStyleInMap = false;
       }
-
       return isExistStyleInMap;
     },
 
-    _setRenderPointsChange( {state, commit} ){
-      
-      const inMap = state.inMap;
-      let layerItem = new Layer( inMap );
-  
-      //Удаляем или прогружаем точки изменения слоев, если есть
-      if( state.pointsOfChanges === -1 )
+    //Вывод детектора изменений
+    _setRenderPointsChange( {state, commit}, inPointsOfChanges ) {
+      const inMap = state.curMap;
+      let layerItem = new Layer(inMap);
+      //Удаляем точки изменения слоев, если стоит флаг снятия слоя детектора
+      if( !inPointsOfChanges )
         layerItem.deletePointOfChange();
-        
-      else if ( state.pointsOfChanges.length !== 0  )
-        layerItem.setPointOfChange( state.pointsOfChanges );
-  
-      commit('setCurrentMapValue', { field : 'pointsOfChanges',  value : null });
-  
+      else {
+        if ( inPointsOfChanges.length !== 0  ) {
+          commit( 'setCurrentMapValue', { field : 'loading', value : true });
+          new Promise(resolve=>{
+            resolve(layerItem.setPointsOfChange( inPointsOfChanges ));
+          }).then( ()=> {
+            setTimeout( ()=> {
+              commit( 'setCurrentMapValue', { field : 'loading', value : false });
+            }, 1000);
+          });
+        } else {
+          ext.setErrorNotification( commit, lang.getMessages('pointsChangeError') );
+          //Прячем чекбокс детектора и шкалу, выводим ошибку
+          commit( 'setCurrentMapValue', { field : 'showPointsChangesCheck', value : false });
+          commit( 'setCurrentMapValue', { field : 'showColorScale', value : false });
+        }
+      }
       //Чистим ресурсы 
       layerItem = null;
     },
 
-     //Создаем лэйер и формируем слой для отчета
+    //Создаем лэйер и формируем слой для отчета
     //Так как нам не нужно ничего коммитить и нужно сформировать только один слой
-    _setReportLayer({state}, obj){
-      
+    _setReportLayer({state}, obj) {
       let layerItem = 
         new Layer( obj.snapshotMap, state.mapModel, state.showColorLayer, state.currentMainIndex, state.currentAddIndex, state.opacityOfLayer );
-      
       layerItem.setReportLayer( obj.isColor );
       layerItem = null;
-    },
+    }
 }
 
 export default privateActionsMap;
