1497a19cea226d508694fa2050500720debae557
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2020-06-11 05:31:56 +0300

    add event on moving

--- src/components/Map/Options/ColorScaleOfChanges.vue
+++ src/components/Map/Options/ColorScaleOfChanges.vue
@@ -1,26 +1,18 @@
 <template>
-    <table class='fullSize colorScale' >
-        <tr>
-            <td title="#0404B4" bgcolor="#0404B4">-6</td>
-            <td title="#0000FF" bgcolor="#0000FF">-5</td>
-            <td title="#2E64FE" bgcolor="#2E64FE">-4</td>
-            <td title="#58ACFA" bgcolor="#58ACFA">-3</td>
-            <td title="#81DAF5" bgcolor="#81DAF5">-2</td>
-            <td title="#81F7F3" bgcolor="#81F7F3">-1</td>
-            <td title="#F3F781" bgcolor="#F3F781">1</td>
-            <td title="#F5DA81" bgcolor="#F5DA81">2</td>
-            <td title="#FAAC58" bgcolor="#FAAC58">3</td>
-            <td title="#FE642E" bgcolor="#FE642E">4</td>
-            <td title="#FF0000" bgcolor="#FF0000">5</td>
-            <td title="#B40404" bgcolor="#B40404">>6</td>
-        </tr>
-    </table>
+  <table class="fullSize colorScale">
+    <tr>
+      <td title="#0404B4" bgcolor="#0404B4">-6</td>
+      <td title="#0000FF" bgcolor="#0000FF">-5</td>
+      <td title="#2E64FE" bgcolor="#2E64FE">-4</td>
+      <td title="#58ACFA" bgcolor="#58ACFA">-3</td>
+      <td title="#81DAF5" bgcolor="#81DAF5">-2</td>
+      <td title="#81F7F3" bgcolor="#81F7F3">-1</td>
+      <td title="#F3F781" bgcolor="#F3F781">1</td>
+      <td title="#F5DA81" bgcolor="#F5DA81">2</td>
+      <td title="#FAAC58" bgcolor="#FAAC58">3</td>
+      <td title="#FE642E" bgcolor="#FE642E">4</td>
+      <td title="#FF0000" bgcolor="#FF0000">5</td>
+      <td title="#B40404" bgcolor="#B40404">>6</td>
+    </tr>
+  </table>
 </template>
-
-<script>
-    
-</script>
-
-<style>
-  
-</style>
