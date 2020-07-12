31bf964f8fa8196adb64c4f97e1bd76033c5784f
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2020-07-12 17:17:33 +0300

    add css-modules

--- src/components/Map/Instruments/ObjectsMapTable.vue
+++ src/components/Map/Instruments/ObjectsMapTable.vue
@@ -1,81 +1,114 @@
 <template>
-    <gridFilter v-show='getShowingObjects.showTableMap' :tableName='curTableName' >
-        <table class='fullSize' >
-            <tr>
-                <td align='left' >
-                    <mButton @click='setActionMapObj(false)' > 
-                        <img class='imgInButton' :src="require('content/images/cancel.png')" >
-                    </mButton>
-                </td>
-                <td align='center' >
-                    <mButton @click='setActiveAllDraws()' :class='getClassForBoard("isMarkersActive")' > 
-                        <img class='imgInButton' :src="require('content/images/alldraws.png')" >
-                    </mButton>
-                    <mButton @click='setChangeModeActiveMapObj("isMarkersActive")' :class='getClassForBoard("isMarkersActive")' > 
-                        <img class='imgInButton' :src="require('content/images/point.png')" >
-                    </mButton>
-                    <mButton @click='setChangeModeActiveMapObj("isMeasureActive")' :class='getClassForBoard("isMeasureActive")' > 
-                        <img class='imgInButton' :src="require('content/images/polygon.png')" >
-                    </mButton>
-                    <mButton @click='setActionChangeDrawMode("isDrawActive")' :class='getClassForBoard("isDrawActive")' v-show='getMapObjProperties.isDrawShow' > 
-                        <img class='imgInButton' :src="require('content/images/drawMeasure.png')" >
-                    </mButton>
-                </td>
-                <td align='right' >
-                    <mButton @click='setActionMapObj(true)' > 
-                        <img class='imgInButton' :src="require('content/images/save.png')" >
-                    </mButton>
-                </td>
-            </tr>
-        </table>
-    </gridFilter>
+  <div
+    v-show="getShowingObjects.showTableMap"
+    class="tableMapPos absolute whiteBack radius border point"
+  >
+    <div class="tableMapMain">
+      <div class="flexBetween">
+        <a-button @click="setActionMapObj(false)" icon="rollback"></a-button>
+        <a-input-search
+          class="searchTable"
+          allowClear
+          :placeholder="$lang.messages.filterText"
+          v-model="searchQuery"
+        />
+        <a-button @click="setActionMapObj(true)" icon="save"></a-button>
+      </div>
+      <GridTable
+        :tableNameInState="tableName"
+        :columnsData="columnsData"
+        :rowsData="rowsData"
+        :filterKey="searchQuery"
+      />
+    </div>
+  </div>
 </template>
-
 <script>
-    import { mapGetters, mapMutations, mapActions } from 'vuex';
-    import gridFilter from 'components/Additional/Table/GridFilter';
-    import mButton from 'components/Additional/MButton';
-    export default {
-        components : { gridFilter, mButton },
-        props : [ 'tableName' ],
-        data :()=>({
-            modalSureSaveName : '',
-        }),
-        created() {
-            this.curTableName = this.tableName;
-        },
-        computed : {
-            ...mapGetters([ 
-                'getShowingObjects', 
-                'getInstrument', 
-                'getClassForBoard', 
-                'getMapObjProperties' 
-            ]),
-        },
-        methods : {
-            ...mapActions([ 'setActionChangeDrawMode' ]),
-            ...mapMutations([ 'setCurrentMapValue', 'setShowMode' ]),
-            setActionMapObj( isSaveAction ) {
-                this.modalSureSaveName = isSaveAction ? this.$lang.messages.sureSaveMapObj : this.$lang.messages.sureCancelMapObj;
-                this.setCurrentMapValue({ field : 'activeModal', value : true });
-                this.setCurrentMapValue({ field : 'activeModalTitle', value : this.modalSureSaveName });  
-                this.setCurrentMapValue({ 
-                    field : 'activeConfrmDialogAction', 
-                    value : {
-                        nameAction : 'setConfirmActionMapObj',
-                        dataAction : isSaveAction
-                    } 
-                });
-            },
-            setChangeModeActiveMapObj( inStateElem ) {
-                this.setShowMode(inStateElem);
-                this.getInstrument.setImagesForDrawObjects();
-            },
-            setActiveAllDraws() {
-                this.setChangeModeActiveMapObj("isMarkersActive");
-                this.setChangeModeActiveMapObj("isMeasureActive");
-                this.setActionChangeDrawMode("isDrawActive");
-            }
+import GridTable from "../../Additional/Table/GridTable.vue";
+import { mapGetters, mapMutations } from "vuex";
+
+export default {
+  components: { GridTable },
+  props: ["tableName"],
+  data: () => ({
+    searchQuery: "",
+    actualRowsData: null
+  }),
+  computed: {
+    ...mapGetters(["getMapObjList", "getLineOrPolygon", "getShowingObjects"]),
+    columnsData() {
+      return [
+        { isOrder: true, value: "" },
+        { isOrder: true, value: this.$lang.messages.tableHeaderName },
+        { isOrder: true, value: this.$lang.messages.tableHeaderDate },
+        { isOrder: true, value: "" },
+        { isOrder: true, value: "" },
+        { isOrder: false, value: "" }
+      ];
+    },
+    rowsData() {
+      // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+      this.actualRowsData = this.getMapObjList.mapObjListServer || [];
+      const finalList = [];
+      this.actualRowsData.forEach(x => {
+        if (x.properties && x.properties.id) {
+          const currType = x.typeView || x.type;
+          const currIsReport = this.getLineOrPolygon(currType)
+            ? x.isReport
+            : true;
+          const currNumColor =
+            x.properties.numColor || (this.getLineOrPolygon(currType) ? 3 : 6);
+          const finalObj = {
+            type: x.properties.type,
+            name: x.properties.name,
+            date: x.properties.date,
+            numColor: currNumColor,
+            isReport: currIsReport,
+            delete: null,
+            id: x.id
+          };
+          finalList.push(finalObj);
         }
+      });
+      return finalList;
     }
+  },
+  methods: {
+    ...mapMutations(["setStateMapValue", "setShowMode"]),
+    setActionMapObj(isSaveAction) {
+      this.modalSureSaveName = isSaveAction
+        ? this.$lang.messages.sureSaveMapObj
+        : this.$lang.messages.sureCancelMapObj;
+      this.setStateMapValue({ field: "activeModal", value: true });
+      this.setStateMapValue({
+        field: "activeModalTitle",
+        value: this.modalSureSaveName
+      });
+      this.setStateMapValue({
+        field: "activeConfrmDialogAction",
+        value: {
+          nameAction: "setConfirmActionMapObj",
+          dataAction: isSaveAction
+        }
+      });
+    }
+  }
+};
 </script>
+<style>
+.tableMapPos {
+  top: 12%;
+  right: 4.5%;
+}
+.tableMapPos > .tableMapMain {
+  min-width: 350px;
+  max-width: 500px;
+  height: calc(70vh);
+  overflow-y: auto;
+
+  padding: 10px;
+}
+.searchTable.searchTable {
+  width: 75%;
+}
+</style>
