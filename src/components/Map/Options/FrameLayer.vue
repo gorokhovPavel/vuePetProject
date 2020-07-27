60939d8c4df1a3fe21c36d7fffae51982c54d291
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2020-07-27 09:47:18 +0300

    add incidents

--- src/components/Map/Options/FrameLayer.vue
+++ src/components/Map/Options/FrameLayer.vue
@@ -1,60 +1,101 @@
 <template>
-    <md-card md-with-hover class='fix' v-show="inShowParam" :style="{ width : inWidth+'px' }" >    
-        <md-card-header class='frameLayerHeader' align="right" >
-            <table class='fullSize' >
-                <tr>
-                    <td>
-                        <mButton
-                            v-show='isSave' 
-                            @click='setClickSaveBtn()' > 
-                                <img class='imgInButton' :src="require('content/images/save.png')" >
-                        </mButton>
-                    </td>
-                    <td align='right'>
-                        <mButton
-                            @click='setShowMode(showName)'
-                            v-show='isExit' > 
-                                <img class='imgInButton' :src="require('content/images/close.png')" >
-                        </mButton>
-                    </td>
-                </tr>
-            </table>
-        </md-card-header>
-        <md-card-expand class='frameLayerContent fullSize' >
-            <slot/>
-        </md-card-expand>        
-   </md-card>
+  <div
+    class="frameLayerContainer fix whiteBack radius border point"
+    v-show="inShowParam"
+    :style="{ width: inWidth + 'px' }"
+  >
+    <div class="frameLayerHeader fullSize allScroll">
+      <a-button v-show="isSave" icon="save" @click="setClickSaveBtn()" />
+      <a-button
+        v-show="isEdit"
+        icon="retweet"
+        class="leftMargin"
+        @click="setClickEditBtn()"
+      />
+      <span v-show="headerText">{{ headerText }}</span>
+      <a-button
+        class="right"
+        icon="close"
+        @click="setShowMode(showName)"
+        v-show="isExit"
+      />
+    </div>
+    <div class="frameLayerContent fullSize">
+      <slot />
+    </div>
+  </div>
 </template>
 
 <script>
-    import { mapMutations, mapActions } from 'vuex'
-    import mButton from 'components/Additional/MButton'
-    import Mover from 'services/Mover'
-    export default {
-        components : { mButton },
-        props : [ 'inWidth', 'inShowParam', 'showName', 'isExit', 'isSave' ],
-        mounted : ()=> {
-            Mover.moveAllObj();
-        },
-        methods : {
-            ...mapMutations(['setShowMode', 'setCurrentMapValue']),
-            setClickSaveBtn() {
-                this.setShowMode('activeModal');
-                this.setCurrentMapValue({ field : 'activeModalTitle', value : this.$lang.messages.objectDialogInfo });
-                this.setCurrentMapValue({ 
-                    field : 'activeConfrmDialogAction', 
-                    value : {
-                        nameAction : 'setStartSaveModal',
-                        //dataAction : isSaveAction
-                    } 
-                });
-            }
+import { mapMutations, mapGetters } from "vuex";
+import { addExtension, mover } from "../../../utils";
+
+export default {
+  props: [
+    "inWidth",
+    "inShowParam",
+    "showName",
+    "isExit",
+    "isSave",
+    "isEdit",
+    "headerText"
+  ],
+  computed: {
+    ...mapGetters(["getMapObjList"])
+  },
+  mounted: () => {
+    mover.moveAllObj();
+  },
+  methods: {
+    ...mapMutations(["setShowMode", "setStateMapValue"]),
+    setClickSaveBtn() {
+      this.setShowMode("activeModal");
+      this.setStateMapValue({
+        field: "activeModalTitle",
+        value: this.$lang.messages.objectDialogInfo
+      });
+      this.setStateMapValue({
+        field: "activeConfrmDialogAction",
+        value: {
+          nameAction: "setStartSaveModal",
+          nameObject: '"qawedw"'
         }
+      });
+    },
+    setClickEditBtn() {
+      const inSelObjDraw = addExtension.getSelectDraw(
+        this.getMapObjList.mapObjListDraw
+      );
+
+      inSelObjDraw.isRecount = true;
+      this.setShowMode("activeModal");
+      this.setStateMapValue({
+        field: "activeModalTitle",
+        value: this.$lang.messages.objectDialogEditInfo
+      });
+
+      this.setStateMapValue({
+        field: "activeConfrmDialogAction",
+        value: {
+          nameAction: "setActionMathCalc",
+          dataAction: inSelObjDraw
+        }
+      });
     }
+  }
+};
 </script>
 <style>
-    .frameLayerHeader, .frameLayerContent{ 
-        padding : 3px;
-        cursor : all-scroll;
-    }
+.frameLayerContainer {
+  z-index: 10;
+}
+.frameLayerHeader {
+  padding: 10px;
+}
+.frame3dPos > .frameLayerHeader {
+  padding: 5px 5px 33px;
+}
+.frameLayerContent {
+  padding: 2px;
+}
 </style>
