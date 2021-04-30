1a1d53904eac72176d7475508824a5cc94423f7e
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2021-04-30 11:38:01 +0300

    Update MeetupAgendaItem.js

--- 2-module/2-task/MeetupAgendaItem.js
+++ 2-module/2-task/MeetupAgendaItem.js
@@ -2,7 +2,6 @@ import { agendaItemTitles, agendaItemIcons } from './data.js';
 
 export const MeetupAgendaItem = {
   name: 'MeetupAgendaItem',
-
   template: `<div class="meetup-agenda__item">
       <div class="meetup-agenda__item-col">
         <img class="icon" alt="icon" v-bind:src="\`/assets/icons/icon-\${processedAgenda.defaultIcon}.svg\`" />
@@ -12,13 +11,11 @@ export const MeetupAgendaItem = {
 
         <h5 class="meetup-agenda__title" v-if="agendaItem.title"> {{agendaItem.title}}</h5>
         <h5 class="meetup-agenda__title" v-else>{{processedAgenda.defaultTitle}}</h5>
-
         <p v-if="agendaItem.type === 'talk' " >
           <span v-if="agendaItem.speaker">{{ agendaItem.speaker }}</span>
           <span v-if="agendaItem.speaker" class="meetup-agenda__dot"></span>
           <span class="meetup-agenda__lang" v-if="agendaItem.language">{{agendaItem.language}}</span>
         </p>
-
         <p v-if="agendaItem.description">{{agendaItem.description}}</p>
       </div>
     </div>`,
