8f68e73c4ef8e84b430f874271bc6a0830521d87
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2021-05-12 10:51:43 +0300

    Update meetup.js

--- data/meetup.js
+++ data/meetup.js
@@ -45,7 +45,7 @@ export default {
   }, {
     "id": 39,
     "startsAt": "10:40",
-    "endsAt": "11:10",
+    "endsAt": "11:00",
     "type": "talk",
     "title": "All you need is <s>love</s> Apollo Client",
     "description": "While we usually mention Apollo Client only in connection with GraphQL, it can do a lot more things to your Vue application such as accessing REST endpoints and replacing Vuex in managing application state! In this talk, I will cover these advanced cases while also explaining the basics of using Apollo with GraphQL endpoints",
