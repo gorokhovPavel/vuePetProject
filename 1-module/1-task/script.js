62052f7134d5e72ca6002c0b7f5a8782e568337f
gorokhovPavel <36862528+gorokhovPavel@users.noreply.github.com>
2021-05-21 17:22:41 +0300

    Update script.js

--- 1-module/1-task/script.js
+++ 1-module/1-task/script.js
@@ -2,11 +2,9 @@ import Vue from '/vendor/vue.esm.browser.js';
 
 const app = new Vue({
   el: "#app",
-
   data: {
     buttonNumber: 0
   },
-
   methods: {
     onclick() {
       ++this.buttonNumber;
